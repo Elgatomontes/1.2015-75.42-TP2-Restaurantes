40fb98a3e7c3cc75c88b9c5a6cc664de0c5795ea
Gastón Montes <gaston.montes@hinch.as>
2015-05-02 01:50:29 -0300

    TP2 - Create project.

---
+++ TP2 - Restaurantes/TP2 - Restaurantes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TP2 - Restaurantes
+//
+//  Created by Gastón Montes on 5/2/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
