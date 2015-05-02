4e950fa14c93cb35c213bd4324787034a42f0b0f
Gastón Montes <gaston.montes@hinch.as>
2015-05-02 15:54:09 -0300

    TP2 - Create tp strcuture.

---
+++ TP2 - Restaurantes/TP2 - Restaurantes/client_main.cpp
@@ -0,0 +1,9 @@
+//
+//  client_main.cpp
+//  TP2 - Restaurantes
+//
+//  Created by Gastón Montes on 5/2/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ TP2 - Restaurantes/TP2 - Restaurantes/server_main.cpp
@@ -0,0 +1,9 @@
+//
+//  server_main.cpp
+//  TP2 - Restaurantes
+//
+//  Created by Gastón Montes on 5/2/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#include <stdio.h>
