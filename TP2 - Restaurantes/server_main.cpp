774e08e419ff11b910809e8595e2787828ddf369
Gaston <gastonmontes@hotmail.com>
2015-05-10 17:54:57 -0300

    TP2 - A simple server.

--- TP2 - Restaurantes/server_main.cpp
+++ TP2 - Restaurantes/server_main.cpp
@@ -8,8 +8,28 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <strings.h>
+#include <string>
+
+#include "server_socket.h"
+
+#define SERVER_PORT 4321
+#define SERVER_BACKLOG 20
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World Server!\n";
+	ServerSocket serverSockFd;
+
+	printf("Bindeando al puerto: %d\n", SERVER_PORT);
+	serverSockFd.socketBind(SERVER_PORT);
+
+	printf("Escuchando conexiones...\n");
+	serverSockFd.socketListenConnections(SERVER_BACKLOG);
+
+	printf("Aceptando conexiones...\n");
+	int clientSocketFd = serverSockFd.socketAcceptConnection();
+
+	printf("Enviando datos...\n");
+	serverSockFd.socketSend(clientSocketFd, "Reciví tu conexión\n");
+
     return 0;
 }
