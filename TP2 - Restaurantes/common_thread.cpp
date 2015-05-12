1dee375323b70a377ec4249b72fd374ae48f6150
Gastón Montes <gaston.montes@hinch.as>
2015-05-12 02:39:48 -0300

    TP2 - Add thread class.

---
+++ TP2 - Restaurantes/common_thread.cpp
@@ -0,0 +1,9 @@
+//
+//  common_thread.cpp
+//  TP2 - Restaurantes
+//
+//  Created by Gastón Montes on 5/12/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#include "common_thread.h"
---
+++ TP2 - Restaurantes/common_thread.h
@@ -0,0 +1,21 @@
+//
+//  common_thread.h
+//  TP2 - Restaurantes
+//
+//  Created by Gastón Montes on 5/12/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#ifndef __TP2___Restaurantes__common_thread__
+#define __TP2___Restaurantes__common_thread__
+
+#include <stdio.h>
+
+class Thread {
+private:
+    
+public:
+
+};
+
+#endif /* defined(__TP2___Restaurantes__common_thread__) */
